7fc1d53b9f19b08e2db84d3afbee10d11e09b8f6
Gavin Castaneda <gcastaneda456@gmail.com>
2026-05-31 22:02:40 -0600

    Initial Commit

---
+++ cron/cronApp.swift
@@ -0,0 +1,15 @@
+//
+//  cronApp.swift
+//  cron
+//
+//  Created by Gavin on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct cronApp: App {
+    var body: some Scene {
+        MenuBarExtra("Cron", systemImage: "hammer") {}
+    }
+}
---
+++ cronTests/cronTests.swift
@@ -0,0 +1,17 @@
+//
+//  cronTests.swift
+//  cronTests
+//
+//  Created by Gavin on 5/31/26.
+//
+
+import Testing
+@testable import cron
+
+struct cronTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
